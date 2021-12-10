55a0cb9bd4a3ce2f53e1d3768604dffde0496a32
Sukairo-02 <reka.serzh@yandex.ru>
2021-12-10 17:57:04 +0200

    Removed unnecessary options

--- src/database/dbObject.ts
+++ src/database/dbObject.ts
@@ -1,10 +1,6 @@
 import { MongoClient } from 'mongodb';
 import config from 'config';
 
-const dbClient = new MongoClient(config.get('db.conStr'), {
-	// @ts-ignore
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-});
+const dbClient = new MongoClient(config.get('db.conStr'));
 
 export = dbClient;
